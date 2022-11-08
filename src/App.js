9e586dd7f28d73f6bbbde0276ee2d61582546455
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-08 12:09:36 -0800

    got text to render on localhost3000

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Homepage from "./pages/Homepage";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
-import Resume from "./pages/Resume";
+import Home from "./pages/Home";
+// import Contact from "./pages/Contact";
+// import About from "./pages/About";
+// import Portfolio from "./pages/Portfolio";
+// import Resume from "./pages/Resume";
 
 import Header from "./components/Headers";
 import Footer from "./components/Footers";
+
 
 function App() {
   return (
@@ -16,10 +17,7 @@ function App() {
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
-            <Routes>
-              <Route path="/" element={<Homepage />} />
-              <Route path="/aboutme" element={<About />} />
-            </Routes>
+
           </div>
           <Footer />
         </div>
