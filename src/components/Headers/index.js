38162e6b93aa3f3b900164f28839bbea3d1a62ed
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-08 12:44:40 -0800

    fixed links in header to go to the appropriate section

--- src/components/Headers/index.js
+++ src/components/Headers/index.js
@@ -14,16 +14,16 @@ const Header = () => {
         </div>
         <div>
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
+              <Link className="btn btn-lg btn-info m-2" to="/about">
                 About Me
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
+              <Link className="btn btn-lg btn-light m-2" to="/portfolio">
                 Portfolio
               </Link>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
+              <Link className="btn btn-lg btn-info m-2" to="/contact">
                 Contact
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
+              <Link className="btn btn-lg btn-light m-2" to="/resume">
                 Resume
               </Link>
             </>
--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@
 //     <div>
 //       <div className="flex-row justify-center mb-3">
 //         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-//           About Mallory.
+//           My name is Mallory Sheehan and I'm 33 years old, living in Los Angeles, California. I'm originally from New Jersey and I graduated from Penn State University in 2011. I started my career at CBS Radio in New York selling local radio advertisements on 1010 WINS and WFAN, as well as homepage takeovers and banners on cbsnewyork.com. I developed an interest in digital advertising, so after my time at CBS, I spent a year selling in-app advertising at Jun Group and most recently, I spent 4 1/2 years at AnyClip selling their proprietary A.I. video technology to digital publishers and media companies. In my free time, I love to go to concerts and travel to new places. I'm excited to be in this course and to learn how to become a developer, so that I can continue my career in tech!
 //         </h2>
 //       </div>
 //     </div>
