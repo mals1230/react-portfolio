f56ae3f7a6c3227dd267b2b6f61a8521f1812b24
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-08 13:48:36 -0800

    added photo to aboutMe page ... now working on getting it to render

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,10 +5,15 @@ const About = () => {
   return (
     <div>
       <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+        <h2 className="col-12 col-md-9 bg-dark text-light p-3 mb-5">
           <h2> About Me: </h2>
           <p> My name is Mallory Sheehan and I'm 33 years old, living in Los Angeles, California. I'm originally from New Jersey and I graduated from Penn State University in 2011. I started my career at CBS Radio in New York selling local radio advertisements on 1010 WINS and WFAN, as well as homepage takeovers and banners on cbsnewyork.com. I developed an interest in digital advertising, so after my time at CBS, I spent a year selling in-app advertising at Jun Group and most recently, I spent 4 1/2 years at AnyClip selling their proprietary A.I. video technology to digital publishers and media companies. In my free time, I love to go to concerts and travel to new places. I'm excited to be a graduate of the UCLA Full Stack Coding Bootcamp and continue my tech career on the development side! </p>
         </h2>
+
+        <h3 className="col-12 col-md-3 bg-dark text-light p-3 mb-5">
+            <img src="./assets/mal.png" alt="Mallory Sheehan"/>
+        </h3>
+
       </div>
     </div>
   );
