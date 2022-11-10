2def459fb3ff70b213d3b49b261858af8e865ee2
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-10 12:34:01 -0500

    got password generator photo to render! saving and doing the same for all other photos

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,64 +1,65 @@
 import React from "react";
-
+import { Card } from "react-bootstrap"
+import password from "./password.png"
 
 const Portfolio = () => {
 
   return (
     <div>
       <div className="flex-row justify-center mb-3">
-        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+        <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Event Fan Follow</header>
           <img className="flex-row justify-center col-12" src="./public/eff.png" alt="Event Fan Follow" height="150"
           />
           <a className="liveLink" href="https://aasports89.github.io/Event-Fan-Follow/">Deployed App</a>
           |
           <a className="liveLink" href="https://github.com/AASports89/Event-Fan-Follow">GitHub Repo</a>
-        </card>
+        </Card>
 
-        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+        <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Book Club</header>
           <img className="flex-row justify-center col-12" src="./public/bookclub.png" alt="Book Club" height="150"
           />
           <a className="liveLink" href="https://secret-harbor-07074.herokuapp.com/">Deployed App</a>
           |
           <a className="liveLink" href="https://github.com/latifah2022/book-club">GitHub Repo</a>
-        </card>
+        </Card>
 
-        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+        <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Take A Hike</header>
           <img className="flex-row justify-center col-12" src="./public/takeahike.png" alt="Take A Hike" height="150"
           />
           <a className="liveLink" href="https://safe-anchorage-34793.herokuapp.com/">Deployed App</a>
           |
           <a className="liveLink" href="https://github.com/mals1230/dog-walk">GitHub Repo</a>
-        </card>
+        </Card>
 
-        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+        <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Work Day Scheduler</header>
           <img className="flex-row justify-center col-12" src="./public/scheduler.png" alt="Work Day Scheduler" height="150"
           />
           <a className="liveLink" href="https://mals1230.github.io/work-day-schedule/">Deployed App</a>
           |
           <a className="liveLink" href="https://github.com/mals1230/work-day-schedule">GitHub Repo</a>
-        </card>
+        </Card>
 
-        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+        <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Random Password Generator</header>
-          <img className="flex-row justify-center col-12" src="./public/password.png" alt="Random Password Generator" height="150"
+          <img className="flex-row justify-center col-12" src={password} alt="Random Password Generator" height="150"
           />
           <a className="liveLink" href="https://mals1230.github.io/random-password-generator/">Deployed App</a>
           |
           <a className="liveLink" href="https://github.com/mals1230/random-password-generator">GitHub Repo</a>
-        </card>
+        </Card>
 
-        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+        <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Code Quiz</header>
           <img className="flex-row justify-center col-12" src="./public/codequiz.png" alt="Code Quiz" height="150"
           />
           <a className="liveLink" href="https://mals1230.github.io/code-quiz/">Deployed App</a>
           |
           <a className="liveLink" href="https://github.com/mals1230/code-quiz">GitHub Repo</a>
-        </card>
+        </Card>
 
       </div>
     </div>
