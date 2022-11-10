0d29a41d49946b130476502241fa38c84b29ed85
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-10 10:54:18 -0500

    adding projects to portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,13 +1,49 @@
 import React from "react";
 
+
 const Portfolio = () => {
 
   return (
     <div>
       <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-9 bg-dark text-light p-3 mb-5">
-          <p> This page is under construction. Check back soon! </p>
-        </h2>
+        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+          <header className="flex-row justify-center col-12">Event Fan Follow</header>
+          <img className="flex-row justify-center col-12" src="./public/eff.png" alt="Event Fan Follow" height="150"
+          />
+          <a className="liveLink" href="https://aasports89.github.io/Event-Fan-Follow/">Deployed App</a>
+          |
+          <a className="liveLink" href="https://github.com/AASports89/Event-Fan-Follow">GitHub Repo</a>
+        </card>
+
+        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+        <header className="flex-row justify-center col-12">Book Club</header>
+          <img className="flex-row justify-center col-12" src="./public/eff.png" alt="Book Club" height="150"
+          />
+          <a className="liveLink" href="https://aasports89.github.io/Event-Fan-Follow/">Deployed App</a>
+          |
+          <a className="liveLink" href="https://github.com/AASports89/Event-Fan-Follow">GitHub Repo</a>
+        </card>
+
+        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+        <header className="flex-row justify-center col-12">Take A Hike</header>
+          <img className="flex-row justify-center col-12" src="./public/eff.png" alt="Take A Hike" height="150"
+          />
+          <a className="liveLink" href="https://aasports89.github.io/Event-Fan-Follow/">Deployed App</a>
+          |
+          <a className="liveLink" href="https://github.com/AASports89/Event-Fan-Follow">GitHub Repo</a>
+        </card>
+
+        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+          text test
+        </card>
+
+        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+          text test
+        </card>
+
+        <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
+          text test
+        </card>
 
       </div>
     </div>
