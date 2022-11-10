8c80092544572f2daf224d4ddc6941aebe7a6fc9
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-10 12:57:58 -0500

    got all images to render on portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Card } from "react-bootstrap"
+import eff from "./eff.png"
+import bookclub from "./bookclub.png"
+import takeahike from "./takeahike.png"
+import scheduler from "./scheduler.png"
 import password from "./password.png"
+import codequiz from "./codequiz.png"
 
 const Portfolio = () => {
 
@@ -9,7 +14,7 @@ const Portfolio = () => {
       <div className="flex-row justify-center mb-3">
         <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Event Fan Follow</header>
-          <img className="flex-row justify-center col-12" src="./public/eff.png" alt="Event Fan Follow" height="150"
+          <img className="flex-row justify-center col-12" src={eff} alt="Event Fan Follow" height="150"
           />
           <a className="liveLink" href="https://aasports89.github.io/Event-Fan-Follow/">Deployed App</a>
           |
@@ -18,7 +23,7 @@ const Portfolio = () => {
 
         <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Book Club</header>
-          <img className="flex-row justify-center col-12" src="./public/bookclub.png" alt="Book Club" height="150"
+          <img className="flex-row justify-center col-12" src={bookclub} alt="Book Club" height="150"
           />
           <a className="liveLink" href="https://secret-harbor-07074.herokuapp.com/">Deployed App</a>
           |
@@ -27,7 +32,7 @@ const Portfolio = () => {
 
         <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Take A Hike</header>
-          <img className="flex-row justify-center col-12" src="./public/takeahike.png" alt="Take A Hike" height="150"
+          <img className="flex-row justify-center col-12" src={takeahike} alt="Take A Hike" height="150"
           />
           <a className="liveLink" href="https://safe-anchorage-34793.herokuapp.com/">Deployed App</a>
           |
@@ -36,7 +41,7 @@ const Portfolio = () => {
 
         <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Work Day Scheduler</header>
-          <img className="flex-row justify-center col-12" src="./public/scheduler.png" alt="Work Day Scheduler" height="150"
+          <img className="flex-row justify-center col-12" src={scheduler} alt="Work Day Scheduler" height="150"
           />
           <a className="liveLink" href="https://mals1230.github.io/work-day-schedule/">Deployed App</a>
           |
@@ -54,7 +59,7 @@ const Portfolio = () => {
 
         <Card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
           <header className="flex-row justify-center col-12">Code Quiz</header>
-          <img className="flex-row justify-center col-12" src="./public/codequiz.png" alt="Code Quiz" height="150"
+          <img className="flex-row justify-center col-12" src={codequiz} alt="Code Quiz" height="150"
           />
           <a className="liveLink" href="https://mals1230.github.io/code-quiz/">Deployed App</a>
           |
