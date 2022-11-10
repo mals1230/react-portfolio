7caa4001f08c983401c0a19653b71874c635a534
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-10 11:15:59 -0500

    added homeworks to portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -16,33 +16,48 @@ const Portfolio = () => {
         </card>
 
         <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
-        <header className="flex-row justify-center col-12">Book Club</header>
-          <img className="flex-row justify-center col-12" src="./public/eff.png" alt="Book Club" height="150"
+          <header className="flex-row justify-center col-12">Book Club</header>
+          <img className="flex-row justify-center col-12" src="./public/bookclub.png" alt="Book Club" height="150"
           />
-          <a className="liveLink" href="https://aasports89.github.io/Event-Fan-Follow/">Deployed App</a>
+          <a className="liveLink" href="https://secret-harbor-07074.herokuapp.com/">Deployed App</a>
           |
-          <a className="liveLink" href="https://github.com/AASports89/Event-Fan-Follow">GitHub Repo</a>
+          <a className="liveLink" href="https://github.com/latifah2022/book-club">GitHub Repo</a>
         </card>
 
         <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
-        <header className="flex-row justify-center col-12">Take A Hike</header>
-          <img className="flex-row justify-center col-12" src="./public/eff.png" alt="Take A Hike" height="150"
+          <header className="flex-row justify-center col-12">Take A Hike</header>
+          <img className="flex-row justify-center col-12" src="./public/takeahike.png" alt="Take A Hike" height="150"
           />
-          <a className="liveLink" href="https://aasports89.github.io/Event-Fan-Follow/">Deployed App</a>
+          <a className="liveLink" href="https://safe-anchorage-34793.herokuapp.com/">Deployed App</a>
           |
-          <a className="liveLink" href="https://github.com/AASports89/Event-Fan-Follow">GitHub Repo</a>
+          <a className="liveLink" href="https://github.com/mals1230/dog-walk">GitHub Repo</a>
         </card>
 
         <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
-          text test
+          <header className="flex-row justify-center col-12">Work Day Scheduler</header>
+          <img className="flex-row justify-center col-12" src="./public/scheduler.png" alt="Work Day Scheduler" height="150"
+          />
+          <a className="liveLink" href="https://mals1230.github.io/work-day-schedule/">Deployed App</a>
+          |
+          <a className="liveLink" href="https://github.com/mals1230/work-day-schedule">GitHub Repo</a>
         </card>
 
         <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
-          text test
+          <header className="flex-row justify-center col-12">Random Password Generator</header>
+          <img className="flex-row justify-center col-12" src="./public/password.png" alt="Random Password Generator" height="150"
+          />
+          <a className="liveLink" href="https://mals1230.github.io/random-password-generator/">Deployed App</a>
+          |
+          <a className="liveLink" href="https://github.com/mals1230/random-password-generator">GitHub Repo</a>
         </card>
 
         <card className="flex-row justify-center col-12 col-md-4 mb-3 p-3">
-          text test
+          <header className="flex-row justify-center col-12">Code Quiz</header>
+          <img className="flex-row justify-center col-12" src="./public/codequiz.png" alt="Code Quiz" height="150"
+          />
+          <a className="liveLink" href="https://mals1230.github.io/code-quiz/">Deployed App</a>
+          |
+          <a className="liveLink" href="https://github.com/mals1230/code-quiz">GitHub Repo</a>
         </card>
 
       </div>
